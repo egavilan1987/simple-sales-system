13fb0fbe3ee88946dc8442ada21c82ef5f3727b6
egavilan1987 <enmanuelgc@gmail.com>
2019-05-21 17:08:04 -0400

    Create productsReport.php

---
+++ productsReport.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<head>
+		<title>Simple Sale System</title>		
+	</head>  
+<ul>
+  <a href="index.html">Home</a>&nbsp; 
+  <a href="newSales.php">New Sales</a>&nbsp;
+  <a href="salesReport.php"> Sales Report</a>&nbsp; 
+  <a href="addClients.php">Add Clients</a>&nbsp; 
+  <a href="clientsReport.php">Clients Report</a>&nbsp; 
+  <a href="addProducts.php">Add Producs</a>&nbsp; 
+</ul>
+
+</body>
+</html>
