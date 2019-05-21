3bca318fb3f9d2dee1b93613ae63cd5b4c87e1e3
egavilan1987 <enmanuelgc@gmail.com>
2019-05-21 16:56:08 -0400

    Create help.php

---
+++ help.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<head>
+		<title>Sistema de Ventas</title>		
+	</head>  
+	<br>
+    
+<table width='500' border='3' align='center'>
+      	<tr>
+			<th bgcolor='silver' colspan='5'>Sales System</th>
+		</tr>
+</table>
+<ul>
+  <li><a href="newSales.php">New Sales</a></li>
+  <li><a href="salesReport.php">Sales Report</a></li>
+  <li><a href="addClients.php">Add Clients</a></li>
+  <li><a href="clientsReport.php">Clients Report</a></li>
+  <li><a href="addProducts">Add Producs</a></li>
+  <li><a href="productsReport">Products Report</a></li>
+</ul>
+
+</body>
+</html>
+
+<ul>
+  <a href="index.html">Home</a>
+  <a href="newSales.php"> New Sales</a>
+  <a href="salesReport.php"> Sales Report</a>
+  <a href="addClients.php">Add Clients</a>
+  <a href="clientsReport.php">Clients Report</a>
+  <a href="addProducts">Add Producs</a>
+  <a href="productsReport">Products Report</a>
+</ul>
