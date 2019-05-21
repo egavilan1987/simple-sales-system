0ee869ce53ccdbe26e5b7008d990ec52f0914a38
egavilan1987 <enmanuelgc@gmail.com>
2019-05-21 16:59:55 -0400

    Update help.php

--- help.php
+++ help.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
-<html>
-<body>
-	<head>
-		<title>Sistema de Ventas</title>		
-	</head>  
-	<br>
-    
-<table width='500' border='3' align='center'>
-      	<tr>
-			<th bgcolor='silver' colspan='5'>Sales System</th>
-		</tr>
-</table>
-<ul>
-  <li><a href="newSales.php">New Sales</a></li>
-  <li><a href="salesReport.php">Sales Report</a></li>
-  <li><a href="addClients.php">Add Clients</a></li>
-  <li><a href="clientsReport.php">Clients Report</a></li>
-  <li><a href="addProducts">Add Producs</a></li>
-  <li><a href="productsReport">Products Report</a></li>
-</ul>
+VERY SIMPLE SALES SYSTEM IN PHP, HML, CSS AND MYSQL
 
-</body>
-</html>
+https://www.w3schools.com/css/tryit.asp?filename=trycss_navbar_basic_html
+http://php.egavilanmedia.com/Ventas/index.php
 
-<ul>
-  <a href="index.html">Home</a>
-  <a href="newSales.php"> New Sales</a>
-  <a href="salesReport.php"> Sales Report</a>
-  <a href="addClients.php">Add Clients</a>
-  <a href="clientsReport.php">Clients Report</a>
-  <a href="addProducts">Add Producs</a>
-  <a href="productsReport">Products Report</a>
-</ul>
+
+
+
