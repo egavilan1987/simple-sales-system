082148d4f57246fa29a5669e163e9cb9f0c2208a
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 15:18:10 -0400

    Update productsManagement.php

--- productsManagement.php
+++ productsManagement.php
@@ -13,6 +13,37 @@
   <a href="clientsReport.php">Clients Report</a>&nbsp; 
   <a href="addProducts.php">Add Producs</a>&nbsp; 
 </ul>
+		<table align='center' width='1000' border='3'>
+			<tr>
+			<td colspan='20' align='center' bgcolor='silver'>
+			Products Management</td>
+			</tr>
 
-</body>
+			<tr align='center'>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Quantity</th>
+				<th>Price</th>
+			</tr>
+			
+		<?php
+		$conn = mysqli_connect("localhost","root","","sales");
+		if(!$conn){
+			die("Connection failed: ".mysql_connect_error());
+		}
+		$sql = "SELECT * FROM products";
+		$query = mysqli_query($conn,$sql) or die("Bad Query: $sql");
+
+		while ($row=mysqli_fetch_assoc($query))
+			{		
+			?>
+			<tr align="center">
+			<td><?php echo $row['id_product']; ?>s</td>
+			<td><?php echo $row['name']; ?>d</td>
+			<td><?php echo $row['quantity']; ?>d</td>
+			<td><?php echo $row['price']; ?>d</td>
+			</tr>
+		<?php } ?>
+		</table>
+	</body>
 </html>
