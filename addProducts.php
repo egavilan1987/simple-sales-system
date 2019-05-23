16801a8d5fc7d705eb02d5be01a27a584b6c321d
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 14:36:29 -0400

    Update addProducts.php

--- addProducts.php
+++ addProducts.php
@@ -21,25 +21,21 @@
 		<tr >
 			<td align='right'>Name:</td>
 			<td><input type='text' name='name' required maxlength="50">
-			</font>
 			</td>
 		</tr>
 		<tr>
 			<td align='right'>Description:</td>
 			<td><input type='text' name='description' required maxlength="500">
-			</font>
 			</td>
 		</tr>
 		<tr>
 			<td align='right'>Quantity :</td>
 			<td><input type='number' name='quantity' min = 0 required>
-			</font>
 			</td>
 		</tr>
 		<tr>
 			<td align='right'>Price: </td>
 			<td><input type='number' name='price' min = 0 required step=".01">
-			</font>
 			</td>
 		</tr>
 		<tr>
