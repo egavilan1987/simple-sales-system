20ef5b5ddb17c7596d491029b4cc585284392594
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 14:34:15 -0400

    Update addProducts.php

--- addProducts.php
+++ addProducts.php
@@ -1,54 +1,54 @@
 <!DOCTYPE html>
 <html>
-<body>
 	<head>
 		<title>Simple Sale System</title>		
 	</head>  
-<ul>
-  <a href="index.html">Home</a>&nbsp; 
-  <a href="newSales.php">New Sales</a>&nbsp;
-  <a href="salesReport.php"> Sales Report</a>&nbsp; 
-  <a href="addClients.php">Add Clients</a>&nbsp; 
-  <a href="clientsReport.php">Clients Report</a>&nbsp; 
-  <a href="productsReport.php">Products Report</a>
-</ul>
+<body>
+	<ul>
+	  <a href="index.html">Home</a>&nbsp; 
+	  <a href="newSales.php">New Sales</a>&nbsp;
+	  <a href="salesReport.php"> Sales Report</a>&nbsp; 
+	  <a href="addClients.php">Add Clients</a>&nbsp; 
+	  <a href="clientsReport.php">Clients Report</a>&nbsp; 
+	  <a href="productsReport.php">Products Report</a>
+	</ul>
 
-<form method='post' action='productsRegistration.php'>
-<table width='500' border='3' align='center'>
-      	<tr>
-			<th bgcolor='silver' colspan='5'>Products Registration Form</h>
-		</tr>
-  		<tr >
-			<td align='right'>Name:</td>
-			<td><input type='text' name='name' required maxlength="50">
-			</font>
-			</td>
-		</tr>
+	<form method='post' action='productsRegistration.php'>
+	<table width='500' border='3' align='center'>
 		<tr>
-			<td align='right'>Description:</td>
-			<td><input type='text' name='description' required maxlength="500">
-			</font>
-			</td>
-		</tr>
-		<tr>
-			<td align='right'>Quantity :</td>
-			<td><input type='number' name='quantity' min = 0 required>
-			</font>
-			</td>
-		</tr>
-		<tr>
-			<td align='right'>Price: </td>
-			<td><input type='number' name='price' min = 0 required step=".01">
-			</font>
-			</td>
-		</tr>
-		<tr>
-			<td align='center' colspan='6'>
-			<input type='submit' name='submit' value='Submit'>
-			</td>
-		</tr>  
-  </table>  
-</form><br>
+				<th bgcolor='silver' colspan='5'>Products Registration Form</h>
+			</tr>
+			<tr >
+				<td align='right'>Name:</td>
+				<td><input type='text' name='name' required maxlength="50">
+				</font>
+				</td>
+			</tr>
+			<tr>
+				<td align='right'>Description:</td>
+				<td><input type='text' name='description' required maxlength="500">
+				</font>
+				</td>
+			</tr>
+			<tr>
+				<td align='right'>Quantity :</td>
+				<td><input type='number' name='quantity' min = 0 required>
+				</font>
+				</td>
+			</tr>
+			<tr>
+				<td align='right'>Price: </td>
+				<td><input type='number' name='price' min = 0 required step=".01">
+				</font>
+				</td>
+			</tr>
+			<tr>
+				<td align='center' colspan='6'>
+				<input type='submit' name='submit' value='Submit'>
+				</td>
+			</tr>  
+	  </table>  
+	</form><br>
 </body>
 
 </html>
