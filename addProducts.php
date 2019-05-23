83ed3c717c79c31ddfbf2d48f83c5c1fb37a8a07
egavilan1987 <enmanuelgc@gmail.com>
2019-05-23 14:32:56 -0400

    Update addProducts.php

--- addProducts.php
+++ addProducts.php
@@ -13,5 +13,84 @@
   <a href="productsReport.php">Products Report</a>
 </ul>
 
+<form method='post' action='productsRegistration.php'>
+<table width='500' border='3' align='center'>
+      	<tr>
+			<th bgcolor='silver' colspan='5'>Products Registration Form</h>
+		</tr>
+  		<tr >
+			<td align='right'>Name:</td>
+			<td><input type='text' name='name' required maxlength="50">
+			</font>
+			</td>
+		</tr>
+		<tr>
+			<td align='right'>Description:</td>
+			<td><input type='text' name='description' required maxlength="500">
+			</font>
+			</td>
+		</tr>
+		<tr>
+			<td align='right'>Quantity :</td>
+			<td><input type='number' name='quantity' min = 0 required>
+			</font>
+			</td>
+		</tr>
+		<tr>
+			<td align='right'>Price: </td>
+			<td><input type='number' name='price' min = 0 required step=".01">
+			</font>
+			</td>
+		</tr>
+		<tr>
+			<td align='center' colspan='6'>
+			<input type='submit' name='submit' value='Submit'>
+			</td>
+		</tr>  
+  </table>  
+</form><br>
 </body>
+
 </html>
+
+
+<?php
+$conn = mysqli_connect("localhost","root","","sales");
+if(!$conn){
+	die("Connection failed: ".mysql_connect_error());
+}
+if(isset($_POST['submit']))
+{
+
+	 $name = $_POST['name'];
+	 $description = $_POST['description'];
+	 $quantity = $_POST['quantity'];
+	 $price = $_POST['price'];
+     
+$sql = "INSERT INTO products(name, description, quantity, price,created_date) VALUES('$name', '$description','$quantity','$price',NOW())";
+
+$query=mysqli_query($conn,$sql);
+
+if($query){
+		echo "<center><b>New Product Details:</b></center><br>";
+		echo 
+		"<table align='center' border='4'>
+			</tr>
+			<tr bgcolor='silver' align='center'>
+				<th>Name</th>
+				<th>Description</th>
+				<th>Quantity</th>
+				<th>Price</th>
+			</tr>
+			<tr>
+				<td>$name</td>
+				<td>$description</td>
+				<td>$quantity</td>
+				<td>$price</td>
+			</tr>
+		</table>";
+		}
+	}
+?>
+
+</body>
