b8666bb746e3d18a7969804713d08702b6b55161
egavilan1987 <enmanuelgc@gmail.com>
2019-05-24 12:12:13 -0400

    Update addClients.php

--- addClients.php
+++ addClients.php
@@ -8,10 +8,10 @@
 <ul>
   <a href="index.html">Home</a>&nbsp; 
   <a href="newSales.php">New Sales</a>&nbsp;
-  <a href="salesReport.php"> Sales Report</a>&nbsp; 
-  <a href="clientsReport.php">Clients Report</a>&nbsp; 
+  <a href="salesManagement.php"> Sales Management</a>&nbsp; 
+  <a href="clientsManagement.php">Clients Management</a>&nbsp; 
   <a href="addProducts.php">Add Producs</a>&nbsp; 
-  <a href="productsReport.php">Products Report</a>
+  <a href="productsManagement.php">Products Management</a>
 </ul>
 
 	<form method='post' action='productsRegistration.php'>
