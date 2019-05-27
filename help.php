b124262fb91840077da72698e5ab9b7c37f8085b
egavilan1987 <enmanuelgc@gmail.com>
2019-05-27 12:32:54 -0400

    Update help.php

--- help.php
+++ help.php
@@ -4,5 +4,5 @@ https://www.w3schools.com/css/tryit.asp?filename=trycss_navbar_basic_html
 http://php.egavilanmedia.com/Ventas/index.php
 
 
-
-
+https://codecanyon.net/item/simple-sales-inventory-system-pos/22533347
+https://codecanyon.net/item/microelephant-simple-point-of-sale/21139713
