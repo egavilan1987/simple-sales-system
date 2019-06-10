d2005efe2cfe4ca1e132a4f5ff8abe520e3eb59b
egavilan1987 <enmanuelgc@gmail.com>
2019-06-09 23:59:06 -0400

    help file last update.

--- help.php
+++ help.php
@@ -6,3 +6,85 @@ http://php.egavilanmedia.com/Ventas/index.php
 
 https://codecanyon.net/item/simple-sales-inventory-system-pos/22533347
 https://codecanyon.net/item/microelephant-simple-point-of-sale/21139713
+
+----------------------------------------------------------------------------------
+This tutorial will cover creating a login system with registration upon registering an activation link will be emailed containing a link to activate the account. Once active the user can login, a reset option is available to reset the password.
+
+Also ensure your php version is at least 5.3.17 but ideally should be 5.6 or 7
+
+----------------------------------------------------------------------------------
+In this tutorial, I walk you through the complete process of creating a user registration system where users can create an account by providing username, email and password, login and logout using PHP and MySQL. I will also show you how you can make some pages accessible only to logged in users. Any other user not logged in will not be able to access the page.
+
+If you prefer a video, you can watch it on my YouTube channel here.
+
+----------------------------------------------------------------------------------
+
+Simple Sales System - PHP and MySQL
+
+Microelephant Simple Point of Sale is a simple yet very useful web based software, suitable for small organizations. This application will come in handy for organizations that deal with multiple units for each item and looking for an easy to use solution.
+
+Features:
+
+Create Items (Products & Services)
+Provision for multiple Unit of Measurement for each item (This option is helpful when you purchase in one UOM, but sell in a different UOM)
+Sales Tax & Discount for each item
+Stock Sheet
+Manage Inventory (Add or adjust inventory items)
+For items with multiple units, upon adding it to inventory or selling it out, in any unit, the system will automatically adjust the stocks for the rest of its related units accordingly
+Nice and easy to use Point of Sale Interface with Receipt printing option
+Automatically rounds up price in Point of Sale
+Option to setup your own Sales Receipt number and Currency
+Add customer without leaving POS Screen
+Create New Users (Roles: 1. Administrator 2. Sales Person)
+Create Expense Category and Enter your Expenses
+Informative Dashboard
+Verify your balance in cash drawer with the system calculated amount based on your sales to find any discrepancy
+Reports:
+Sales Summary
+Sales Summary by Item
+Inventory Valuation
+Inventory Movement
+Income/Profit or Loss Statement
+Sales Tax Liability
+Price Round
+Expenses
+Languages:
+English
+Arabic
+German
+Spanish
+French
+Portuguese
+Brazilian Portuguese (Pt-BR)
+Built using Laravel Framework, version 5.4 
+
+Live Demo
+
+------------------------------------------------------------------------------------------------
+
+Simple sales - Inventory System & POS
+
+
+Point Of Sale is a laravel project which is as known as POS. POS is a very easy handy tools. This project can be used in any kind of retail shop, where buy and sell process. Point of Sales System is easy to use and able to handle multiple bills and search opened bill option and search product from POS and able to make Purchase Order to Supplier and coupon Card and able to Export Report by pdf Format and Profit & Loss and more. Easy to setup the application.Multi Outlets POS can be used online, Mobile Device, iPads, Android tablets or laptops. Responsive on any Devices.
+
+Features :
+
+Fully 100% Responsive and Dynamic;
+Nice role management;
+Bulk product list upload in xlsx;
+Warehouse and lot management system;
+More secure platform;
+Shows All brands and supplier list;
+Admin can able to set vat/ tax globally or to a single product;
+The system can generate coupon and can export as pdf;
+Every product has a unique barcode sign;
+Daily, Monthly and yearly details report of buy, sell and income;
+Demo URL
+
+Username :admin@email.com
+
+Password :12345
+
+Again, thank you for purchasing Point Of Sale. If you need some help, or support please use email or contact form via codecanyon profile site.
+
+Hope you happy with the POS system, all the best with your business.
